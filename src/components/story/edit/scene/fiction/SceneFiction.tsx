938f5d1fceb2d1fba37d7d660de7db97e928a074
richdouglasevans <rich.douglas.evans@gmail.com>
2025-05-30 13:07:48 +0100

    Syntax highlighting and error highlighting
    * Headings are rendered in bold
    * Links are in a different color to plain text
    * Errors are highlighted

--- src/components/story/edit/scene/fiction/SceneFiction.tsx
+++ src/components/story/edit/scene/fiction/SceneFiction.tsx
@@ -1,16 +1,21 @@
 /** biome-ignore-all assist/source/organizeImports: don't care */
 import { useMutation } from "@tanstack/react-query";
-import { type FC, useState } from "react";
+import {
+  type FC,
+  type KeyboardEventHandler,
+  useCallback,
+  useState,
+} from "react";
 import { useTranslation } from "react-i18next";
-import { type Descendant, Node, createEditor } from "slate";
+import { createEditor, Editor, Node, Transforms } from "slate";
 import { withHistory } from "slate-history";
 import { Editable, Slate, withReact } from "slate-react";
 
 import { ErrorCodedError } from "@domain/error/ErrorCodedError";
+import { ErrorCodes } from "@domain/error/errorCode";
 import type { PersistentScene, PersistentStory } from "@domain/index";
 import unsupported from "@domain/story/client/unsupportedResult";
-
-import styles from "./SceneFiction.module.css";
+import parse from "@domain/story/publish/parse/parse";
 
 import {
   type WithDeleteScene,
@@ -19,7 +24,11 @@ import {
   useEnvironment,
 } from "../../context/ClientContext";
 import type { OnSceneChanged } from "../types";
+import Block from "./block/Block";
+import getContent from "./getContent";
+import styles from "./SceneFiction.module.css";
 import Toolbar from "./Toolbar";
+import toSlateModel, { emptyBlock } from "./toSlateModel";
 
 type SceneFictionProps = {
   storyId: PersistentStory["id"];
@@ -40,20 +49,18 @@ const SceneFiction: FC<SceneFictionProps> = ({
 
   const [editor] = useState(() => withReact(withHistory(createEditor())));
 
-  const [editorState] = useState([
-    {
-      children: [{ text: scene.content }],
-    },
-  ]);
+  const [editorState] = useState(toSlateModel(scene));
 
   const saveTheSceneContent = useMutation<string, Error, string>({
     mutationFn: (content) =>
       saveSceneContent({ storyId, sceneId: scene.id, content }).then(
         (result) => {
           switch (result.kind) {
-            case "sceneContentSaved":
+            case "sceneContentSaved": {
               return content;
-            case "error":
+            }
+
+            case "error": {
               return Promise.reject(
                 new ErrorCodedError(
                   result.error.errorCode,
@@ -61,8 +68,11 @@ const SceneFiction: FC<SceneFictionProps> = ({
                   { cause: result.error.context },
                 ),
               );
-            default:
+            }
+
+            default: {
               return unsupported(result);
+            }
           }
         },
       ),
@@ -110,6 +120,59 @@ const SceneFiction: FC<SceneFictionProps> = ({
     }
   };
 
+  const renderBlock = useCallback(Block, []);
+
+  const onKeyDown: KeyboardEventHandler = (e) => {
+    if (e.key === "Enter" && editor.selection) {
+      // grab the current line
+      const currentNode = Editor.node(editor, editor.selection, { depth: 1 });
+
+      // and grab that line's text
+      const text = Node.string(currentNode[0]).trim();
+
+      if (text.length > 0) {
+        // parse the line to see what kind of line it is
+        const result = parse(text, 1);
+
+        switch (result.kind) {
+          case "link": {
+            Transforms.setNodes(editor, { type: "blockLink" });
+            break;
+          }
+
+          case "headerNamed":
+          case "headerAnonymous": {
+            Transforms.setNodes(editor, { type: "blockHeading" });
+            break;
+          }
+
+          case "error": {
+            Transforms.setNodes(editor, {
+              type: "blockError",
+              error: {
+                code: result.errorCode || ErrorCodes.Error,
+                message: result.message,
+              },
+            });
+            break;
+          }
+        }
+      }
+
+      e.preventDefault();
+
+      // the user pressed <ENTER> so add an empty block for the new line
+      Transforms.insertNodes(editor, emptyBlock(), {
+        at: editor.selection,
+      });
+      // and move the cursor to that new line
+      Transforms.move(editor, {
+        distance: 1,
+        unit: "word",
+      });
+    }
+  };
+
   return (
     <div className={styles.sceneFiction}>
       <Slate editor={editor} initialValue={editorState}>
@@ -138,7 +201,9 @@ const SceneFiction: FC<SceneFictionProps> = ({
           </button>
         </Toolbar>
         <Editable
+          onKeyDown={onKeyDown}
           className={styles.editor}
+          renderElement={renderBlock}
           readOnly={saveTheSceneContent.isPending}
         />
       </Slate>
@@ -147,11 +212,3 @@ const SceneFiction: FC<SceneFictionProps> = ({
 };
 
 export default SceneFiction;
-
-function getContent(nodes: Descendant[]): string {
-  let content = nodes.map((n) => Node.string(n)).join("\n");
-  if (!content.endsWith("\n")) {
-    content = `${content}\n`;
-  }
-  return content;
-}
